f166ea52e80eff194901ebe56261546718a6e97d
Jonathan Redwine <jredwine1@gmail.com>
2019-10-09 19:05:11 -0400

    separated classes into their own files

---
+++ Tile.cpp
@@ -0,0 +1,30 @@
+//
+//  Tile.cpp
+//  
+//
+//  Created by Jonathan Redwine on 10/9/19.
+//
+
+#include <stdio.h>
+using namespace std;
+
+
+class Tile {
+public:
+    int isEntrance;
+    int isBreezy;
+    int isSmelly;
+    int hasWumpus;
+    int hasGold;
+    int hasPit;
+    int playerBeenHere;
+    Tile(int e, int b, int s, int w, int g, int p, int pbh) {
+        isEntrance = e;
+        isBreezy = b;
+        isSmelly = s;
+        hasWumpus = w;
+        hasGold = g;
+        hasPit = p;
+        playerBeenHere = pbh;
+    }
+};
